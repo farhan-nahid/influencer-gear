57a8a641ba39cea66b24a41d674b9791dc89f547
gesora96 <nahidahmed9933@gmail.com>
2022-01-15 01:06:59 +0600

    clients added

---
+++ assets/js/index.js
@@ -0,0 +1,11 @@
+$(document).ready(() => {
+  $('#clients__area').slick({
+    slidesToShow: 7,
+    slidesToScroll: 1,
+    arrows: false,
+    speed: 500,
+    centerMode: true,
+    centerPadding: '0',
+    autoplay: true,
+  });
+});
